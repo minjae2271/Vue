641b8aff5e471d168139981c1eb6b11063de49f4
MinJaeGwon <minjae2271@naver.com>
2020-12-22 07:05:15 +0900

    findIndex

--- nodebird/ch1/front/store/posts.js
+++ nodebird/ch1/front/store/posts.js
@@ -5,11 +5,18 @@ export const state = () => ({
 export const mutations = {
     addPost(state, payload){
         state.mainPosts.unshift(payload);
+    },
+    removePost(state, payload){
+        const index = state.mainPosts.findIndex((element) => element.id === payload.id)
+        state.mainPosts.splice(index,1)
     }
 }
 
 export const actions = {
     add({commit}, payload){
         commit('addPost', payload);
+    },
+    remove({commit}, payload){
+        commit('removePost', payload);
     }
 }
